5f997a149c6cb5ec93f3e23dfad553821786653c
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-08-30 22:25:04 +0530

    Packages: Errors init

---
+++ packages/errors/src/index.ts
@@ -0,0 +1,6 @@
+export * from './bad-request';
+export * from './not-found';
+export * from './unauthenticated';
+export * from './custom-error';
+export * from './zod-error';
+export * from './database';
