c2cb46f8c9464d9be43ee47caa72e2d4cdec5df7
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-08-31 19:07:54 +0530

    Server: Database loader

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,17 +1,19 @@
 import expressLoader from './loaders/express';
 import config from './config';
+import { connectAndLog } from './loaders/database';
 
 const { PORT, NODE_ENV } = config;
 
 (async () => {
   const app = await expressLoader();
   try {
+    await connectAndLog();
     app.listen(PORT, async () => {
       console.log(
         `${NODE_ENV?.toLocaleUpperCase()} Server Listening at PORT: ${PORT}`
       );
     });
   } catch (err) {
-    console.error('Could not connect to the server');
+    console.error('Could not connect to the server:', err);
   }
 })();
