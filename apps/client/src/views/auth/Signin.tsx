f9b430eef9d6f81e29b5f4e42c2a372096ce35ca
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-09-01 00:41:17 +0530

    Client: Auth ui

---
+++ apps/client/src/views/auth/Signin.tsx
@@ -0,0 +1,105 @@
+import { Link } from 'react-router-dom';
+import { ChangeEvent, useState, FormEvent } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import {
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  Input,
+  Label
+} from '@/components/ui';
+import services from '@/services/auth.route';
+import { LoginUser } from '@e_commerce_package/models/types';
+
+export const SigninForm = () => {
+  const queryClient = useQueryClient();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const [loginData, setLoginData] = useState<LoginUser>({
+    email: '',
+    password: ''
+  });
+
+  const { mutateAsync: signInMutation } = useMutation({
+    mutationFn: (data: LoginUser) => services.signIn(data),
+    onSettled() {
+      queryClient.invalidateQueries({ queryKey: ['auth', 'status'] });
+      setLoading(false);
+    }
+  });
+
+  const handleFormChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+    setLoginData((prev) => ({
+      ...prev,
+      [name]: value
+    }));
+  };
+
+  const handleSignIn = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    signInMutation(loginData);
+  };
+
+  return (
+    <div className='h-full flex flex-col justify-center items-center'>
+      <Card className='mx-auto max-w-sm'>
+        <CardHeader>
+          <CardTitle className='text-2xl'>Sign In</CardTitle>
+          <CardDescription>
+            Enter your email below to sign in to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSignIn} className='grid gap-4'>
+            <div className='grid gap-2'>
+              <Label htmlFor='email'>Email</Label>
+              <Input
+                id='email'
+                name='email'
+                type='email'
+                value={loginData.email}
+                onChange={handleFormChange}
+                placeholder='m@example.com'
+                required
+              />
+            </div>
+            <div className='grid gap-2'>
+              <div className='flex items-center'>
+                <Label htmlFor='password'>Password</Label>
+                <Link
+                  to='/forgot-password'
+                  className='ml-auto inline-block text-sm underline'
+                >
+                  Forgot your password?
+                </Link>
+              </div>
+              <Input
+                id='password'
+                name='password'
+                type='password'
+                value={loginData.password}
+                placeholder='********'
+                onChange={handleFormChange}
+                required
+              />
+            </div>
+            <Button type='submit' className='w-full' isLoading={loading}>
+              Login
+            </Button>
+          </form>
+          <div className='mt-4 text-center text-sm'>
+            Don&apos;t have an account?{' '}
+            <Link to='/sign-up' className='underline'>
+              Sign up
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
