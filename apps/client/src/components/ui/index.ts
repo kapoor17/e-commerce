98bcfb98348abd9a567face5f1c1dfc3cb8bf553
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-09-01 00:40:43 +0530

    Client: UI components init

---
+++ apps/client/src/components/ui/index.ts
@@ -0,0 +1,5 @@
+export * from './button';
+export * from './toast';
+export * from './card';
+export * from './input';
+export * from './label';
---
+++ apps/client/src/components/utility/routes.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '@/providers/AuthProvider';
+import { Else, If, Then } from './conditionals';
+
+export const PrivateRoutes: React.FC = () => {
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) return;
+
+  return (
+    <If condition={isAuthenticated}>
+      <Then>
+        <Outlet />
+      </Then>
+      <Else>
+        <Navigate to='/sign-in' />
+      </Else>
+    </If>
+  );
+};
+
+export const PublicRoutes: React.FC = () => {
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) return;
+
+  return (
+    <If condition={isAuthenticated}>
+      <Then>
+        <Navigate to='/dashboard' />
+      </Then>
+      <Else>
+        <Outlet />
+      </Else>
+    </If>
+  );
+};
