b6abba9d1e4d18e3a71e60c178478defababe700
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-08-31 21:25:02 +0530

    Server: routes init

--- apps/server/src/loaders/routes.ts
+++ apps/server/src/loaders/routes.ts
@@ -1,9 +1,24 @@
 import { Express } from 'express';
 import authRouter from '../routes/auth.route';
+import { isAuthenticated } from '../middlewares';
+import userRouter from '../routes/user.route';
+import productRouter from '../routes/product.route';
+import orderRouter from '../routes/order.route';
+import cartRouter from '../routes/cart.route';
+import cartItemRouter from '../routes/cartItem.route';
+import orderItemRouter from '../routes/orderItem.route';
+import reviewRouter from '../routes/review.route';
 
 const routesLoader = (app: Express) => {
-  app.get('/', (req, res) => res.send('Hello World!'));
-  app.use('/auth', authRouter);
+  app.use('/api/v1/auth', authRouter);
+  app.use(isAuthenticated);
+  app.use('/api/v1/user', userRouter);
+  app.use('/api/v1/product', productRouter);
+  app.use('/api/v1/order', orderRouter);
+  app.use('/api/v1/orderItem', orderItemRouter);
+  app.use('/api/v1/cart', cartRouter);
+  app.use('/api/v1/cartItem', cartItemRouter);
+  app.use('/api/v1/review', reviewRouter);
 };
 
 export default routesLoader;
--- apps/server/src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './user.route';
-export * from './review.route';
-export * from './orderItem.route';
-export * from './product.route';
-export * from './order.route';
-export * from './cartItem.route';
-export * from './cart.route';
