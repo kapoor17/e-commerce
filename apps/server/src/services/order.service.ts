27d4731c5b0d463bc399adfcaae249c420b0b693
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-09-01 23:24:57 +0530

    Server: Order related schema fetching

--- apps/server/src/services/order.service.ts
+++ apps/server/src/services/order.service.ts
@@ -1,9 +1,42 @@
+import { DetailedOrder, OrderInsert } from '@e_commerce_package/models/types';
 import { client } from '../models';
 import { OrderService as BaseOrderService } from '@e_commerce_package/models/services';
 
 class OrderService extends BaseOrderService {
   constructor() {
     super(client);
+  }
+
+  public async findMany(
+    data: Partial<OrderInsert> = {}
+  ): Promise<DetailedOrder[]> {
+    const order = (await super.findMany(data, {
+      with: {
+        orderItems: {
+          with: {
+            product: true
+          }
+        },
+        user: true
+      }
+    })) as DetailedOrder[];
+
+    return order;
+  }
+
+  public async findOne(data: Partial<OrderInsert>): Promise<DetailedOrder> {
+    const order = (await super.findOne(data, {
+      with: {
+        orderItems: {
+          with: {
+            product: true
+          }
+        },
+        user: true
+      }
+    })) as DetailedOrder;
+
+    return order;
   }
 }
 
