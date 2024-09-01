ae541536996e07aaafe4a6b932bc80939a62cb39
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-09-01 18:12:06 +0530

    Client: add to cart and review

--- apps/client/src/views/products/Product.tsx
+++ apps/client/src/views/products/Product.tsx
@@ -10,11 +10,14 @@ import { Button } from '@/components/ui/button';
 import { useParams } from 'react-router-dom';
 import productServices from '@/services/product.route';
 import cartItemServices from '@/services/cartItem.route';
-import cartServices from '@/services/cart.route';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { Skeleton } from '@/components/ui/skeleton';
 import { useState } from 'react';
 import { useAuth } from '@/providers/AuthProvider';
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import Review from '../reviews/Review';
+import { StarFilledIcon, StarIcon } from '@radix-ui/react-icons';
+import { If, Then, Else } from '@/components/utility/conditionals';
 
 export default function Product() {
   const { user } = useAuth();
@@ -47,33 +50,11 @@ export default function Product() {
       onSuccess: () => {
         queryClient.invalidateQueries({ queryKey: ['cartItems', 'getAll'] });
         queryClient.invalidateQueries({ queryKey: ['cart', 'getAll'] });
-      }
+      },
+      mutationKey: ['cartItem', 'createOne']
     });
 
-  const { mutateAsync: createCart } = useMutation({
-    mutationFn: () =>
-      cartServices.createOne({
-        userId: user?.id ?? ''
-      }),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['auth', 'status'] });
-    }
-  });
-
   if (!product || !user) return null;
-
-  async function handleAddToCart(
-    event: React.MouseEvent<HTMLButtonElement>
-  ): Promise<void> {
-    event.preventDefault();
-    if (!user) throw new Error('User not found');
-    if (!product) throw new Error('Product not found');
-    if (!user.cart || !user.cart.id) {
-      await createCart();
-    }
-    await createCartItem();
-    return;
-  }
 
   const averageRating = calculateAverageRating(product.reviews);
 
@@ -95,16 +76,18 @@ export default function Product() {
               </div>
               <div className='flex items-center gap-4'>
                 <div className='flex items-center gap-0.5'>
-                  {Array.from({ length: 5 }, (_, index) => (
-                    <StarIcon
-                      key={index}
-                      className={
-                        index < Math.floor(averageRating)
-                          ? 'w-5 h-5 fill-primary'
-                          : 'w-5 h-5 fill-muted stroke-muted-foreground'
-                      }
-                    />
-                  ))}
+                  {Array.from({ length: 5 }, (_, index) => {
+                    return (
+                      <If condition={index < averageRating}>
+                        <Then>
+                          <StarFilledIcon key={index} />
+                        </Then>
+                        <Else>
+                          <StarIcon key={index} />
+                        </Else>
+                      </If>
+                    );
+                  })}
                 </div>
               </div>
             </div>
@@ -133,7 +116,7 @@ export default function Product() {
             </div>
             <Button
               isLoading={creatingCartItem}
-              onClick={handleAddToCart}
+              onClick={() => createCartItem()}
               size='lg'
             >
               Add to cart
@@ -152,25 +135,21 @@ export default function Product() {
           </div>
         </div>
       </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Reviews</CardTitle>
+        </CardHeader>
+        <CardContent className='grid gap-8'>
+          {product.reviews.map((review) => (
+            <Review
+              key={review.id}
+              user={review.user}
+              rating={review.rating}
+              comment={review.comment}
+            />
+          ))}
+        </CardContent>
+      </Card>
     </Skeleton>
-  );
-}
-
-function StarIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
-      viewBox='0 0 24 24'
-      fill='none'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-    >
-      <polygon points='12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2' />
-    </svg>
   );
 }
