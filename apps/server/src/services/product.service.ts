9405ddbe52cdccf06e1328f1ee77de159ad19c99
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-09-01 17:24:21 +0530

    Server: fetching product with reviews

--- apps/server/src/services/product.service.ts
+++ apps/server/src/services/product.service.ts
@@ -1,9 +1,33 @@
+import {
+  ProductInsert,
+  ProductWithReview
+} from '@e_commerce_package/models/types';
 import { client } from '../models';
 import { ProductService as BaseProductService } from '@e_commerce_package/models/services';
 
 class ProductService extends BaseProductService {
   constructor() {
     super(client);
+  }
+
+  public async findOne(
+    data: Partial<ProductInsert>
+  ): Promise<ProductWithReview> {
+    const product = (await super.findOne(data, {
+      with: {
+        reviews: {
+          with: {
+            user: {
+              columns: {
+                first_name: true
+              }
+            }
+          }
+        }
+      }
+    })) as ProductWithReview;
+
+    return product;
   }
 }
 
